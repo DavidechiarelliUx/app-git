5cb79812c55c5c614c0558be67e0969bc2f562f1
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-06-28 17:29:04 +0200

    chore:project init

---
+++ public/logo.jsx
@@ -0,0 +1,225 @@
+
+export const SvgComponent = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={1396} height={1973}>
+      <path
+        fill="none"
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={3}
+        d="M693 1409v-104"
+      />
+      <path
+        fill="#14044d"
+        fillOpacity={0.796}
+        fillRule="evenodd"
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m693 849 540 241-540 319Z"
+      />
+      <linearGradient
+        id="a"
+        x1={154}
+        x2={693}
+        y1={1129}
+        y2={1129}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#716b94" />
+        <stop offset={1} stopColor="#37367b" />
+      </linearGradient>
+      <filter
+        id="b"
+        width={1396}
+        height={1973}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+        primitiveUnits="userSpaceOnUse"
+      >
+        <feGaussianBlur stdDeviation={14} />
+        <feOffset dx={11.308} dy={9.856} result="offsetblur" />
+        <feFlood floodColor="#000" floodOpacity={0.5} />
+        <feComposite in2="offsetblur" operator="in" />
+        <feMerge>
+          <feMergeNode />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <path
+        fill="url(#a)"
+        fillOpacity={0.796}
+        fillRule="evenodd"
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M693 849v560l-539-318Z"
+        filter="url(#b)"
+      />
+      <filter
+        id="c"
+        width={1396}
+        height={1973}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+        primitiveUnits="userSpaceOnUse"
+      >
+        <feGaussianBlur stdDeviation={32} />
+        <feOffset dx={13.315} dy={4.326} result="offsetblur" />
+        <feFlood floodColor="#000" floodOpacity={0.5} />
+        <feComposite in2="offsetblur" operator="in" />
+        <feMerge>
+          <feMergeNode />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <path
+        fill="#37367b"
+        fillOpacity={0.797}
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M693 1409v441l539-760Z"
+        filter="url(#c)"
+      />
+      <filter
+        id="d"
+        width={1396}
+        height={1973}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+        primitiveUnits="userSpaceOnUse"
+      >
+        <feGaussianBlur stdDeviation={70} />
+        <feOffset dx={11.413} dy={3.708} result="offsetblur" />
+        <feFlood floodColor="#000" floodOpacity={0.5} />
+        <feComposite in2="offsetblur" operator="in" />
+        <feMerge>
+          <feMergeNode />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <path
+        fill="#716b94"
+        fillOpacity={0.801}
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m154 1091 539 759v-441Z"
+        filter="url(#d)"
+      />
+      <linearGradient
+        id="e"
+        x1={154}
+        x2={1232}
+        y1={1129}
+        y2={1129}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#716b94" />
+        <stop offset={1} stopColor="#37367b" />
+      </linearGradient>
+      <filter
+        id="f"
+        width={1396}
+        height={1973}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+        primitiveUnits="userSpaceOnUse"
+      >
+        <feGaussianBlur stdDeviation={60} />
+        <feOffset dx={9} result="offsetblur" />
+        <feFlood floodColor="#000" floodOpacity={0.5} />
+        <feComposite in2="offsetblur" operator="in" />
+        <feMerge>
+          <feMergeNode />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <path
+        fill="url(#e)"
+        fillOpacity={0.816}
+        fillRule="evenodd"
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m693 849 539 241-539 319-539-318 539-242Z"
+        filter="url(#f)"
+      />
+      <path
+        fill="#37367b"
+        fillOpacity={0.919}
+        fillRule="evenodd"
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m693 745 539 242-539 317-539-317 539-242Z"
+      />
+      <filter
+        id="g"
+        width={1396}
+        height={1973}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+        primitiveUnits="userSpaceOnUse"
+      >
+        <feGaussianBlur stdDeviation={32} />
+        <feOffset dy={14} result="offsetblur" />
+        <feFlood floodColor="#000" floodOpacity={0.5} />
+        <feComposite in2="offsetblur" operator="in" />
+        <feMerge>
+          <feMergeNode />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <path
+        fill="#37367b"
+        fillOpacity={0.799}
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M1232 987 693 92v1213Z"
+        filter="url(#g)"
+      />
+      <filter
+        id="h"
+        width={1396}
+        height={1973}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+        primitiveUnits="userSpaceOnUse"
+      >
+        <feGaussianBlur stdDeviation={32} />
+        <feOffset dx={3.188} dy={13.632} result="offsetblur" />
+        <feFlood floodColor="#000" floodOpacity={0.5} />
+        <feComposite in2="offsetblur" operator="in" />
+        <feMerge>
+          <feMergeNode />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <path
+        fill="#716b94"
+        fillOpacity={0.801}
+        stroke="#14044d"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M693 92 154 987l539 318Z"
+        filter="url(#h)"
+      />
+    </svg>
+  )
+  export default SvgComponent
+  
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="app-git"> App git</div>
+    </>
+  );
+}
+
+export default App;
