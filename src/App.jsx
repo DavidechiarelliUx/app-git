767c1b8de9ff9a13c84f39b425b0ccdb17460c23
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-06-28 19:25:36 +0200

    feat: add fitness-section in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import FitnessSection from './components/fitness-section';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div className="app-git"> App git</div>
+     <div className="Title">Training Plan</div>
+    <FitnessSection/>
     </>
   );
 }
